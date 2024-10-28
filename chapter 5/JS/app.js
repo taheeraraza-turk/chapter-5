85a4396fdff5feb7d3462ae6330855971c18d7fa
Taheera Raza Turk <taheeraraza0@gmail.com>
2024-10-28 10:38:33 -0700

    Add files via upload

---
+++ chapter 5/JS/app.js
@@ -0,0 +1,156 @@
+// 1. Write a program that take two numbers & add them in a new variable. Show the result in your browser.
+  //  let num1 = Number.parseInt(prompt("Enter first operand  for addition"));
+  //  let num2 = Number.parseInt(prompt("Enter second operand for addition"));
+  //    let ans = num1 + num2;
+    
+// document.write("The sum of " ,  num1  , " and " ,  num2  , " is " , ans ,"<br>  <br>" );
+
+// 2. Repeat task1 for subtraction, multiplication, division & modulus.
+// let num01 = Number.parseInt(prompt("Enter first operand for subtraction" ));
+// let num02 = Number.parseInt(prompt("Enter second operand for subtraction"));
+//   let ans1 = num01 - num02;
+ 
+// document.write("The subtraction of " ,  num01  , " and " ,  num02  , " is " , ans1 ,"<br> <br>" );
+
+// let numb1 = Number.parseInt(prompt("Enter first operand for multiplication"));
+// let numb2 = Number.parseInt(prompt("Enter second operand for multiplication"));
+//   let ans01 = numb1 * numb2;
+ 
+// document.write("The multiplication of " ,  numb1  , " and " ,  numb2  , " is " , ans01 ,"<br> <br>" );
+
+// let number1 = Number.parseInt(prompt("Enter first operand for division"));
+// let number2 = Number.parseInt(prompt("Enter second operand for division"));
+//   let ans2 = number1 / number2;
+ 
+// document.write("The division of " ,  number1  , " and " ,  number2  , " is " , ans2 ,"<br> <br>" );
+
+// let no1 = Number.parseInt(prompt("Enter first operand for modulus"));
+// let no2 = Number.parseInt(prompt("Enter second operand for modulus"));
+//   let ans3 = no1 / no2;
+ 
+// document.write("The modulus of " ,  no1  , " and " ,  no2  , " is " , ans3 ,"<br> <br>" )
+
+// 3. Do the following using JS Mathematic Expressions
+// a. Declare a variable.
+//     let number ;
+// // b. Show the value of variable in your browser like “Value after variable declaration is: ??”.
+//      document.write("Variable after declaration is" , number  , "<br> <br>");
+// // c. Initialize the variable with some number.
+//     number = 10;
+// // d. Show the value of variable in your browser like “Initial value: 5”.
+//     document.write("Initial value:" , number , "<br> <br>");
+// // e. Increment the variable.
+//    number++;
+// // f. Show the value of variable in your browser like “Value after increment is: 6”.
+//     document.write("Value after increment:" , number , "<br> <br>");
+// // g. Add 7 to the variable.
+//   number = number + 7;
+// // h. Show the value of variable in your browser like “Value after addition is: 13”.
+//       document.write("value after addition of 7 is " , number , "<br> <br>");
+// // i. Decrement the variable.
+//      number--;
+// // j. Show the value of variable in your browser like “Value after decrement is: 12”.
+//       document.write("Value after decrement is:" , number , "<br> <br>");
+// // k. Show the remainder after dividing the variable’s value by 3.
+//     number = number % 3;
+// // l. Output : “The remainder is : 0”.
+//     document.write("The remainder is:" , number , "<br> <br>");
+
+// 4. Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a variable & calculate the cost of buying 5 tickets to a movie. Example output:
+    //  let cost = 600;
+    // alert("The cost of one ticket is 600 PKR ! ");
+    // let tickets = prompt("How many tickets you want?");
+
+    // let total = cost * tickets;
+
+    // document.write("The total cost to buy  " ,   tickets  , " tickets is "  ,   total  )
+
+// 5. Write a script to display multiplication table of any number in your browser. E.g
+
+      //  let table=prompt("Enter the number,you want the table of!!");
+      //  let range=prompt("Enter the range of table!!");
+
+      //  for (let a = 1; a <=range; a++) {
+      //       document.write(table , " X " , a , " = " , table*a , "<br>")
+  
+      //  }
+
+// 6. The Temperature Converter:
+    // let celsius = 25; 
+    // let f = (celsius * 9/5)+32;
+    // document.write(celsius , "degree celsius is " , f  , "fahrenheit <br> <br>");
+
+    // let fahrenheit=70;
+    // let c = (f-32)*5/9;
+    // document.write(fahrenheit , " fahrenheit is "  , c  , " degree celsius")
+
+// question 07
+
+// let colours=650;
+// let book=100;
+// let charges=100;
+
+// let quantity1=prompt("Enter quantity of colours you ordered!");
+// let quantity2=prompt("Enter quantity of books you ordered!");
+
+// let item1=colours*quantity1;
+// let item2 = book * quantity2;
+
+// let total = charges + item1 + item2;
+
+// document.write("The price of colours is :"  ,  colours , " PKR <br> <br> ");
+// document.write("The quantity of Colours you ordered is :"  ,  quantity1  ,  "<br> <br>");
+// document.write("The price of book is :"  ,  book , " PKR <br> <br> ");
+// document.write("The quantity of book you ordered is :"  ,  quantity2  , "<br> <br>");
+// document.write("The shipping charges is:"  ,  charges , " PKR <br> <br>");
+// document.write("The total cost of your order is "  , total  , "PKR")
+
+
+// QUESTION 08
+
+// let totalnum=980;
+// let obtnum = prompt("write your obtained numbers out of 980");
+// let percent=obtnum/totalnum*100;
+// document.write("Total marks:" , totalnum , "<br> <br>");
+// document.write("Obtained marks:" , obtnum , "<br> <br>");
+// document.write("Percentage:" , percent , " % <br> <br>");
+
+
+// QUESTION 09
+
+// let dp=104.80;
+// let rp = 28;
+
+// let dollar = prompt("Change your currency from dollar to pakistani rupee" ,"Enter dollar currency here");
+// let p1=dollar*dp;
+// document.write(dollar , " dollar is equal to "  ,  p1 , "  pakistani rupee  <br> <br>");
+
+// let riyal= prompt("Change your currency from Riyal to pakistani rupee" ,"Enter Riyal currency here");
+// let p2=riyal*rp;
+// document.write(riyal , " riyal is equal to "  ,  p2 , "  pakistani rupee");
+
+// question 10
+
+// let num = 5;
+// num = (num + 5) *10 /2;
+// document.write(num)
+
+// question 11
+
+// let currentyr = 2024;
+// let birthyr = prompt("What is your birth year?");
+
+// let age = currentyr-birthyr;
+
+// document.write("Your age is "  ,  age )
+
+// question 12
+
+// let radius = prompt("what is the radius of the circle?");
+// let circum = 2 * 3.14 * radius;
+// let area = 3.14 * radius**2;
+
+// document.write("Total radius of circle:" , radius , "<br> <br>");
+// document.write("circumference:"  ,  circum  , "<br> <br>" );
+// document.write("area of circle:"  ,  area  , "<br> <br>" );
+ 
